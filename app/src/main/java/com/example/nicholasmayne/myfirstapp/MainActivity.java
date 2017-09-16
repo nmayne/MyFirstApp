b773b4373dfcbfffc2956f34d9ffdb5c981313af
nmayne <nmayne@ualberta.ca>
2017-09-16 10:08:02 -0600

    First Commit

--- app/src/main/java/com/example/nicholasmayne/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/nicholasmayne/myfirstapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.nicholasmayne.myfirstapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        // Do something in response to button
     }
 }
